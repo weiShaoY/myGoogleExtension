7ff5da65d96e400e277f9ab1a146601bc26b7bd2
weiShao <1604705673@qq.com>
2026-03-28 21:20:18 +0800

    chore: 🔧修改文件图案

--- src/content/pages/setting/folderScanner.vue
+++ src/content/pages/setting/folderScanner.vue
@@ -194,44 +194,53 @@ async function mainBtnHandler() {
 </script>
 
 <template>
-  <section
-    class="group relative h-full w-full flex flex-col items-center justify-center"
+
+  <div
+    class="group relative h-[calc(90px*1.7)] min-w-[230px] w-[20%] flex flex-col items-center justify-end rounded-[15px] bg-[linear-gradient(135deg,#6acf63,#52b44b)] p-[10px] shadow-[0_15px_30px_rgba(0,0,0,0.2)]"
   >
+
     <div
-      class="file [perspective:1500px] relative z-50 h-40 w-60 origin-bottom cursor-pointer"
+      class="absolute left-[calc(50%-60px)] top-[-20px] animate-[float_2.5s_infinite_ease-in-out] transition-transform duration-350 ease-in-out group-hover:scale-105"
     >
-      <div
-        class="work-5 relative h-full w-full origin-top rounded-2xl rounded-tl-none bg-amber-600 transition-all duration-300 ease before:[clip-path:polygon(0_35%,0%_100%,50%_100%);] after:absolute before:absolute after:bottom-[99%] after:left-0 before:left-[75.5px] after:h-4 after:w-20 before:h-4 before:w-4 after:rounded-t-2xl after:bg-amber-600 before:bg-amber-600 group-hover:shadow-[0_20px_40px_rgba(0,0,0,.2)] after:content-[''] before:content-[''] before:-top-[15px]"
-      />
 
       <div
-        class="work-4 absolute inset-1 origin-bottom select-none rounded-2xl bg-zinc-400 transition-all duration-300 ease group-hover:[transform:rotateX(-20deg)]"
-      />
+        class="absolute h-[80px] w-[120px] rounded-[10px] bg-[linear-gradient(135deg,#ffe563,#ffc663)] shadow-[0_15px_30px_rgba(0,0,0,0.3)]"
+      >
+        <div
+          class="absolute inset-0 origin-bottom rounded-[15px] bg-white opacity-50 transition-transform duration-350 group-hover:[transform:rotateX(-5deg)_skewX(5deg)]"
+        />
+
+        <div
+          class="absolute inset-0 origin-bottom rounded-[15px] bg-white opacity-50 transition-transform duration-350 group-hover:[transform:rotateX(-15deg)_skewX(12deg)]"
+        />
+      </div>
 
       <div
-        class="work-3 absolute inset-1 origin-bottom rounded-2xl bg-zinc-300 transition-all duration-300 ease group-hover:[transform:rotateX(-30deg)]"
-      />
+        class="relative z-10 origin-bottom transition-transform duration-350 group-hover:[transform:rotateX(-40deg)_skewX(15deg)]"
+      >
+        <div
+          class="absolute top-[-10px] z-20 h-[20px] w-[80px] rounded-t-[12px] bg-[linear-gradient(135deg,#ff9a56,#ff6f56)] shadow-[0_5px_15px_rgba(0,0,0,0.2)]"
+        />
 
-      <div
-        class="work-2 absolute inset-1 origin-bottom rounded-2xl bg-zinc-200 transition-all duration-300 ease group-hover:[transform:rotateX(-38deg)]"
-      />
-
-      <div
-        class="work-1 absolute bottom-0 h-[156px] w-full flex origin-bottom items-end rounded-2xl rounded-tr-none from-amber-500 to-amber-400 bg-gradient-to-t transition-all duration-300 ease before:[clip-path:polygon(100%_14%,50%_100%,100%_100%);] group-hover:[transform:rotateX(-46deg)_translateY(1px)] after:absolute before:absolute after:bottom-[99%] after:right-0 before:right-[142px] before:size-3 after:h-[16px] after:w-[146px] after:rounded-t-2xl after:bg-amber-400 before:bg-amber-400 group-hover:shadow-[inset_0_20px_40px_#fbbf24,_inset_0_-20px_40px_#d97706] after:content-[''] before:content-[''] before:-top-[10px]"
-        @click="mainBtnHandler"
-      />
+        <div
+          class="h-[80px] w-[120px] rounded-[10px] bg-[linear-gradient(135deg,#ffe563,#ffc663)] shadow-[0_15px_30px_rgba(0,0,0,0.3)]"
+        />
+      </div>
     </div>
 
-    <p
-      class="pt-4 text-3xl opacity-20"
+    <label
+      class="relative w-full cursor-pointer rounded-[10px] bg-white/20 px-[35px] py-[10px] text-center text-[1.1em] text-white shadow-[0_10px_20px_rgba(0,0,0,0.1)] transition-colors duration-350 hover:bg-white/40"
+      @click="mainBtnHandler"
     >
+      <input
+        type="file"
+        class="hidden"
+      >
       选择 Emby 文件夹
-    </p>
-  </section>
+    </label>
+  </div>
 
-  <el-timeline
-    class="!w-full"
-  >
+  <el-timeline>
     <el-timeline-item>
       <div
         class="flex items-center"
