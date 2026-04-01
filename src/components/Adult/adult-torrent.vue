008f94541a8a19be924e743332b201cfa43ab0b3
weiShao <1604705673@qq.com>
2026-04-01 15:51:26 +0800

    chore: 🔧暂存

--- src/components/Adult/adult-torrent.vue
+++ src/components/Adult/adult-torrent.vue
@@ -1,5 +1,6 @@
 <!------------------------------------  磁链列表  ------------------------------------------------->
 <script lang="ts" setup>
+import { computed } from 'vue'
 
 import { AdultConfig } from '@/configs'
 
@@ -14,7 +15,6 @@ type PropsType = {
    *  种子列表
    */
   torrentList?: AdultType.TorrentItem[]
-
 }
 
 const props = withDefaults(defineProps<PropsType>(), {
@@ -23,17 +23,11 @@ const props = withDefaults(defineProps<PropsType>(), {
 })
 
 /**
- *  种子列表
- */
-const torrentList = ref<AdultType.TorrentItem[]>(props.torrentList)
-
-console.log('%c Line:39 🍢 torrentList', 'color:#fca650', props.torrentList)
-
-/**
  *  有中文字幕的数量
  */
-const chineseCount = computed(() =>
-  props.torrentList.filter(item => item.tags.includes('tag-ziMu')).length,
+const chineseCount = computed(
+  () =>
+    props.torrentList.filter(item => item.tags.includes('tag-ziMu')).length,
 )
 
 /**
@@ -41,11 +35,11 @@ const chineseCount = computed(() =>
  */
 function copyTorrentUrl(torrent: AdultType.TorrentItem) {
   if (!torrent.url) {
-    window.$notification.error('传入磁链为空, 无法复制,请检查代码')
+    window.$notification?.error('传入磁链为空, 无法复制,请检查代码')
     return
   }
 
-  copyToClipboard(torrent.url, {
+  copyToClipboard?.(torrent.url, {
     title: `${torrent.name} 已复制到剪切板`,
     message: `${torrent.url} `,
   })
@@ -72,72 +66,84 @@ function getTorrentStyle(torrent: AdultType.TorrentItem) {
       }
 }
 
-async function main() {
-  torrentList.value.sort((videoA: AdultType.TorrentItem, videoB: AdultType.TorrentItem) => {
-  /**
-   *   视频A在排序规则数组中的位置   （-1 代表不在数组中）
-   */
-    const indexA = AdultConfig.torrentSortRules.findIndex(rule => videoA.name.includes(rule.name))
+/**
+ * 排序后的种子列表
+ */
+const sortedTorrentList = computed(() => {
+  return [...props.torrentList].sort((videoA, videoB) => {
+    console.log('🚀 ~ file: adult-torrent.vue:74 ~ videoA:', videoA.name)
+    console.log('🚀 ~ file: adult-torrent.vue:74 ~ videoB:', videoB.name)
+
+    /**
+     *   视频A在排序规则数组中的位置   （-1 代表不在数组中）
+     */
+    const indexA = AdultConfig.torrentSortRules.findIndex(rule =>
+      videoA.name.includes(rule.name),
+    )
 
     /**
      *   视频B在排序规则数组中的位置   （-1 代表不在数组中）
      */
-    const indexB = AdultConfig.torrentSortRules.findIndex(rule => videoB.name.includes(rule.name))
+    const indexB = AdultConfig.torrentSortRules.findIndex(rule =>
+      videoB.name.includes(rule.name),
+    )
 
-    // 1.在规则数组中，按数组里关键词的顺序排序,如果关键词的顺序一样了,按文件大小排序
+    console.log('🚀 ~ file: adult-torrent.vue:83 ~ indexA:', indexA)
+    console.log('🚀 ~ file: adult-torrent.vue:83 ~ indexB:', indexB)
 
+    // # ////////////////////////////////////////  1.在规则数组中，按数组里关键词的顺序排序,如果关键词的顺序一样了,按文件大小排序  ////////////////////////////////////////
     // videoA在规则数组中 videoB不在规则数组中 则videoA应该排在videoB前面  应该返回 -1
     if (indexA !== -1 && indexB === -1) {
       return -1
     }
 
+    // videoA不在规则数组中，videoB在，videoB排前面
     //  videoA不在规则数组中 videoB在规则数组中 则videoB应该排在videoA前面  应该返回 1
     if (indexA === -1 && indexB !== -1) {
       return 1
     }
 
-    //  videoA 和 videoB 都在规则数组中
+    // 两者都在规则数组中
     if (indexA !== -1 && indexB !== -1) {
-      // 如果 关键词顺序一样，则按文件大小排序
+      // 关键词顺序一样，按文件大小排序
       if (indexA === indexB) {
         //  如果 videoA 的 文件大小 大于 videoB 的文件大小，则应该返回 -1 将 videoA 排在 videoB 前面
         return videoA.size > videoB.size ? -1 : 1
       }
-      else {
-        //  如果 关键词顺序不一样，则按关键词顺序排序  从小到大
-        return indexA < indexB ? -1 : 1
-      }
+
+      // 关键词顺序不一样，按关键词顺序排序
+      return indexA < indexB ? -1 : 1
     }
 
-    // 2. 如果文件名是纯小写的，则属于第二大类，按文件大小排序
+    // # ////////////////////////////////////////  2. 文件名是纯小写的，按文件大小排序  ////////////////////////////////////////
     const isLowerCaseA = /^[a-z0-9.-]+$/.test(videoA.name)
 
     const isLowerCaseB = /^[a-z0-9.-]+$/.test(videoB.name)
 
+    // 两者都是纯小写，按文件大小排序
     //  如果 videoA 和 videoB 都是纯小写的，则按文件大小排序
     if (isLowerCaseA && isLowerCaseB) {
       //  如果 videoA 的 文件大小 大于 videoB 的文件大小，则应该返回 -1 将 videoA 排在 videoB 前面
       return videoA.size > videoB.size ? -1 : 1
     }
 
+    // videoA是纯小写，videoB不是，videoA排前面
     //  如果 videoA 是纯小写的，videoB 是不是纯小写的 则将 videoA 排在 videoB 前面
     if (isLowerCaseA && !isLowerCaseB) {
       return -1
     }
 
-    //  如果 videoA 是不是纯小写的，videoB 是纯小写的 则将 videoB 排在 videoA 前面
+    // videoA不是纯小写，videoB是，videoB排前面
+    // 如果 videoA 是不是纯小写的，videoB 是纯小写的 则将 videoB 排在 videoA 前面
     if (!isLowerCaseA && isLowerCaseB) {
       return 1
     }
 
-    // 3. 其他文件属于第三大类，按文件大小排序
+    // # ////////////////////////////////////////  3. 其他情况，按文件大小排序  ////////////////////////////////////////
     // 如果 videoA 的大小 大于 videoB 的大小，则应该返回 -1 将 videoA 排在 videoB 前面
     return videoA.size > videoB.size ? -1 : 1
   })
-}
-
-main()
-
+})
 </script>
 
 <template>
@@ -154,20 +160,15 @@ main()
         <div
           class="flex items-center justify-center text-4 font-semibold text-stroke-black"
         >
-
-          <span>
-            共
-          </span>
+          <span> 共 </span>
 
           <span
             class="m-x-2 text-6 text-[#000] font-bold"
           >
-            {{ torrentList.length }}
+            {{ sortedTorrentList.length }}
           </span>
 
-          <span>
-            部
-          </span>
+          <span> 部 </span>
         </div>
 
         <div
@@ -180,11 +181,8 @@ main()
             {{ chineseCount }}
           </span>
 
-          <span>
-            部有中文字幕
-          </span>
+          <span> 部有中文字幕 </span>
         </div>
-
       </div>
 
       <div
@@ -192,8 +190,8 @@ main()
       >
         <!-- # -----------------------------------------------  列表循环  Start  ------------------------------------------------->
         <div
-          v-for="(torrent, index) in torrentList"
-          :key="index"
+          v-for="(torrent, index) in sortedTorrentList"
+          :key="torrent.name || index"
         >
           <div
             class="group relative z-0 h-[6em] flex cursor-pointer items-center justify-between overflow-hidden rounded-[1em] p-2"
@@ -205,7 +203,6 @@ main()
             <div
               class="absolute z-[-1] h-[5em] w-[5em] rounded-full duration-500 -left-[4.5em] -top-[4.5em] group-hover:scale-[4500%]"
               :style="{
-                // background: 'linear-gradient(to right,#00DFA2,#ADFF2F)',
                 background: 'linear-gradient(to right,#2233AA,#ADFF2F)',
               }"
             />
@@ -222,24 +219,24 @@ main()
                 <div
                   class="truncate text-4 font-bold group-hover:text-[#fff]"
                   :style="{
-                    color: torrent.backgroundColor ? '#fff' : '#000',
+                    color: getTorrentStyle(torrent).backgroundColor
+                      ? '#fff'
+                      : '#000',
                   }"
                 >
-                  {{
-                    torrent.name
-                  }}
+                  {{ torrent.name }}
                 </div>
 
                 <!-- 磁链时间 -->
                 <div
                   class="m-t-1 text-3 font-semibold !group-hover:text-[#fff]"
                   :style="{
-                    color: torrent.backgroundColor ? '#fff' : '#9CA3AF',
+                    color: getTorrentStyle(torrent).backgroundColor
+                      ? '#fff'
+                      : '#9CA3AF',
                   }"
                 >
-                  {{
-                    torrent.time
-                  }}
+                  {{ torrent.time }}
                 </div>
               </div>
 
@@ -251,9 +248,7 @@ main()
                   v-if="torrent.source"
                   class="text-4 font-bold"
                 >
-                  {{
-                    getTorrentStyle(torrent).web
-                  }}
+                  {{ getTorrentStyle(torrent).web }}
                 </span>
               </div>
 
@@ -265,17 +260,13 @@ main()
                   v-if="torrent.size"
                   class="text-4 font-bold"
                 >
-                  {{
-                    torrent.size
-                  }}
+                  {{ torrent.size }}
                 </span>
 
                 <span
                   v-if="torrent.size"
                   class="font-semibold"
-                >
-                  GB
-                </span>
+                > GB </span>
               </div>
 
               <!-- 标签图标 -->
@@ -284,21 +275,17 @@ main()
                 :key="tag"
                 class="m-x-3"
               >
-
                 <SvgIcon
                   :icon="tag"
                   class="!h-10 !w-10"
                 />
-
               </div>
-
             </div>
 
             <!-- 右边 -->
             <div
               class="w-auto p-2"
             >
-
               <el-button
                 @click="copyTorrentUrl(torrent)"
               >
@@ -308,17 +295,13 @@ main()
           </div>
 
           <div
-            v-if="torrentList.length === 1 || (torrentList.length > 1 && index !== torrentList.length - 1)"
+            v-if="index < sortedTorrentList.length - 1"
             class="m-y-2 h-[1px] rounded bg-[#9CA3AF]"
           />
-
         </div>
-
       </div>
     </div>
   </Teleport>
 </template>
 
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>
