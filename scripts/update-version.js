8c05072cd3fee978c8a85a793b766228f9b6b56e
weiShao <1604705673@qq.com>
2026-04-20 16:08:22 +0800

    chore: 🔧测试

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -1,12 +1,13 @@
 import fs from 'node:fs/promises'
-import path from 'node:path'
+
+const APP_VERSION_META_RE = /<meta name="app-version" content=".*?"/
+const INDEX_HTML_URL = new URL('../src/option/index.html', import.meta.url)
 
 async function run() {
-  const file = path.resolve('/src/option/index.html')
   const v = Date.now()
-  let c = await fs.readFile(file, 'utf8')
-  c = c.replace(/<meta name="app-version" content=".*?"/, `<meta name="app-version" content="${v}"`)
-  await fs.writeFile(file, c, 'utf8')
+  let c = await fs.readFile(INDEX_HTML_URL, 'utf8')
+  c = c.replace(APP_VERSION_META_RE, `<meta name="app-version" content="${v}"`)
+  await fs.writeFile(INDEX_HTML_URL, c, 'utf8')
   console.log('✅ 版本已更新')
 }
 run()
